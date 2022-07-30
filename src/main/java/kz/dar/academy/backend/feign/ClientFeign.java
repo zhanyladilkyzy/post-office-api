426d2d8db67134ba8a1720f8d2c06ef589dab808
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2022-07-30 13:01:11 +0600

    adding new requests for ClientFeign

--- src/main/java/kz/dar/academy/backend/feign/ClientFeign.java
+++ src/main/java/kz/dar/academy/backend/feign/ClientFeign.java
@@ -1,11 +1,22 @@
 package kz.dar.academy.backend.feign;
 
+import kz.dar.academy.backend.model.ClientResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @FeignClient("client-core-api")
 public interface ClientFeign {
 
     @GetMapping("/client/check")
     String checkClient();
+
+    @GetMapping("/client/all")
+    List<ClientResponse> getAllClients();
+
+    @GetMapping("/client/{clientId}")
+    ClientResponse getClientById(@PathVariable String clientId);
+
 }
